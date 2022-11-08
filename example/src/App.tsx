ad741202983002b4cd8db5a9cbdade5985b992ec
Hoa Phan <hoaphantn7604@gmail.com>
2022-11-08 21:59:43 +0700

    chore: clean code

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,43 +1,45 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { StyleSheet, View, Text, Alert } from 'react-native';
+import { StyleSheet, View, Text, Alert, ScrollView } from 'react-native';
 import { Image, Avatar } from 'react-native-element-image';
-const img = require('./assets/default.png');
+const img = require('./assets/default.jpg');
 
 const ImageScreen = (_props: any) => {
   return (
-    <View style={styles.container}>
-      <Avatar
-        size={80}
-        containerStyle={styles.avatar}
-        source={img}
-        iconEnable={false}
-        onPressIcon={() => Alert.alert('Click')}
-        nameStyle={{ fontSize: 20 }}
-      />
-      <Avatar
-        size={80}
-        containerStyle={styles.avatar}
-        source={img}
-        iconEnable
-        onPressIcon={() => Alert.alert('Click')}
-        nameStyle={{ fontSize: 20 }}
-      />
+    <ScrollView>
+      <View style={styles.container}>
+        <Avatar
+          size={80}
+          containerStyle={styles.avatar}
+          source={img}
+          iconEnable={false}
+          onPressIcon={() => Alert.alert('Click')}
+          nameStyle={{ fontSize: 20 }}
+        />
+        <Avatar
+          size={80}
+          containerStyle={styles.avatar}
+          source={img}
+          iconEnable
+          onPressIcon={() => Alert.alert('Click')}
+          nameStyle={{ fontSize: 20 }}
+        />
 
-      <Avatar
-        size={100}
-        containerStyle={styles.avatar}
-        source={img}
-        iconEnable
-        onPressIcon={() => Alert.alert('Click')}
-        name="User name"
-        nameStyle={{ fontSize: 20, marginBottom: 20 }}
-      />
-      <Text>Width: 200, Height: Automatic</Text>
-      <Image style={styles.image} source={img} width={200} />
-      <Text style={styles.text}>Width: Automatic, Height: 200</Text>
-      <Image style={styles.image} source={img} height={200} />
-    </View>
+        <Avatar
+          size={100}
+          containerStyle={styles.avatar}
+          source={img}
+          iconEnable
+          onPressIcon={() => Alert.alert('Click')}
+          name="User name"
+          nameStyle={{ fontSize: 20, marginBottom: 20 }}
+        />
+        <Text>Width: 150, Height: Automatic</Text>
+        <Image style={styles.image} source={img} width={150} />
+        <Text style={styles.text}>Width: Automatic, Height: 200</Text>
+        <Image style={styles.image} source={img} height={200} />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -48,6 +50,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingVertical: 20,
   },
   image: {
     margin: 20,
